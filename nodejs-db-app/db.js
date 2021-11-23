1750b9432712461a3c366e6622a390c4cc34e865
ealeixoc <enzo.aleixo-carvalho@efrei.net>
2021-11-23 18:42:19 +0100

    Nodejs project

---
+++ nodejs-db-app/db.js
@@ -0,0 +1,16 @@
+//db.js
+
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: "root",
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+
+//Convert pool object to promise based object
+const promisePool = pool.promise();
+
+module.exports = promisePool;
